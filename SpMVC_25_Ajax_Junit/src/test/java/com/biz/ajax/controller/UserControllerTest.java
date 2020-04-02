8edc6ae6ee1bf08e0a6ff468081b374d7aa25beb
jeongseongjong <jjong9950@naver.com>
2020-04-02 18:02:11 +0900

    test코드 * http 상태코드 공부

--- SpMVC_25_Ajax_Junit/src/test/java/com/biz/ajax/controller/UserControllerTest.java
+++ SpMVC_25_Ajax_Junit/src/test/java/com/biz/ajax/controller/UserControllerTest.java
@@ -12,7 +12,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,10 +23,18 @@ import com.biz.ajax.domain.UserVO;
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
 public class UserControllerTest {
 
+	/*
+	 * 사용자 정의 DI 컨테이너 모드
+	 * 단독모드
+	 * 스프링 MVC의 설정을 적용한 DI 컨테이너를 만들어 이 DI 컨테이너를 사용해
+	 * 스프링 MVC 동작 재현 애플리케이션 서버에 배포한 것과 같은 것 처럼 테스트 가능 
+	 */
 	private MockMvc mockMvc;
 	
+	// 테스트 대상 컨트롤러 지정
 	@InjectMocks
 	private UserController userController;
+	
 	
 	@Before
 	public void setUp() throws Exception {
@@ -48,13 +55,16 @@ public class UserControllerTest {
 		 * 결과가 200인지 검사하고
 		 * 최종적으로 model에 담겨서 리턴되는 값이 userVO냐?
 		 */
+		// 실행할 컨트롤러의 mapping을 지정
 		mockMvc.perform(post("/saveUser")
+				// 값을 넣어줄 VO의 변수지정
 			.param("userId",userVO.sampleVO().getUserId())
 			.param("password",userVO.sampleVO().getPassword())
 			.param("userName",userVO.sampleVO().getUserName())
 			.param("rolle",userVO.sampleVO().getRolle())
 				
 			).andExpect(status().isOk())
+			// 매핑된 컨트롤러에서 userVO라는 ModelAttribute로 보내주는것이 있어야 한다.
 			.andExpect(model().attributeExists("userVO"))
 			.andReturn();
 	}
@@ -67,12 +77,15 @@ public class UserControllerTest {
 				.param("rolle", "admin")
 		)
 		.andExpect(status().isOk())
+		// controller의 매핑된곳에서도 명확하게 type이 지정되어있어야 한다.
 		.andExpect(content().contentType("application/json;charset=UTF-8"))// .APPLICATION_JSON))
+		// userVO의 userId가 존재한다.
 		.andExpect(jsonPath("$.userVO.userId").exists())
+		// userId가 admin이 맞느냐 ? 체크하는 코드
 		.andExpect(jsonPath("$.userVO.userId", is("admin")))
 		.andExpect(jsonPath("$.userVO.password").exists())
 		.andExpect(jsonPath("$.userVO.password", is("1234")))
-		.andExpect(jsonPath("$.userVO.userNam").exists())
+		.andExpect(jsonPath("$.userVO.userName").exists())
 		.andExpect(jsonPath("$.userVO.rolle").exists());
 		
 		
